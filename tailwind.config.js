e84147681f8383bf3c0ea4315e32bbfc3d866e6a
gio <giovannybm@gmail.com>
2021-05-02 20:07:11 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily:{
+         'sans': [ '"Nunito Sans"', 'sans-serif'],
+   },
+   extend: {
+       colors: {
+       "blue-multi-1": "#164263",
+       "blue-multi-2": "#10304c",
+       "blue-multi-2": "#0b2030",
+       "blue-accent-1": "#efefef",
+       "gray-multi-1":"#efefef"
+     },   
+
+   },
+ },
+  variants: {
+    extend: {},   
+  },
+  plugins: [
+  
+  ],
+}
